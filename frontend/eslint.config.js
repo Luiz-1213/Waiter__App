b5e4e58d16a7bebaca41130fcb07685919a36ff6
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-02-19 13:00:46 -0300

    chore: initialize frontend project with Vite and TypeScript
     Set up Vite with React and TypeScript
     Add ESLint configuration
     Configure TypeScript settings (app and node)
     Initialize project structure with index.html and App component
     Update .gitignore and dependencies

---
+++ frontend/eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import pluginReact from "eslint-plugin-react";
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react': pluginReact,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "indent": ["error", 2],                
+      "quotes": ["error", "single"],        
+      "semi": ["error", "always"],           
+      "react/react-in-jsx-scope": "off",
+      "react/function-component-definition": [
+        "error",
+        {
+          "namedComponents": "function-declaration",
+          "unnamedComponents": "function-expression" 
+        }]
+    },
+  },
+)
