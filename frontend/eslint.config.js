5e5df95d77b765d4ca479c387a75568e189f53e8
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-02-20 13:41:02 -0300

    feat: add styled-components and global styles setup
     Install styled-components and configure GlobalStyles
     Update package.json with styled-components dependency

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -35,6 +35,7 @@ export default tseslint.config(
           "namedComponents": "function-declaration",
           "unnamedComponents": "function-expression" 
         }]
+      },
     },
-  },
-)
+  )
+  
